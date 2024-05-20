a712f966aa4cbff62e313f276789a66028168438
LaibaArooj <144527015+LaibaArooj@users.noreply.github.com>
2024-05-20 23:46:35 +0500

    lost

--- bill-splitter.cpp
+++ bill-splitter.cpp
@@ -1,6 +1,28 @@
-#include<iostream>
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <vector>
 using namespace std;
-int main(){
+class BillSplitter
+{
+private:
+  unordered_map<string, double> balances;
 
-return 0;
+public:
+  void addFriend(const string &name)
+  {
+    if (balances.find(name) == balances.end())
+    {
+      balances[name] = 0.0;
+    }
+    else
+    {
+      cout << name << " is already added." << endl;
+    }
+  }
+};
+int main()
+{
+
+  return 0;
 }
