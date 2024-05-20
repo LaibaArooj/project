982a065351b66df76247b43fb2da3f3f74116440
LaibaArooj <144527015+LaibaArooj@users.noreply.github.com>
2024-05-20 22:56:09 +0500

    new branch tsting

--- bill-splitter.cpp
+++ bill-splitter.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
+#include <unordered_map>
+#include <string>
+#include <vector>
 using namespace std;
-int main(){
 
-return 0;
-}
+class BillSplitter {
+private:
+    unordered_map<string, double> balances;
+
+public:
+    void addFriend(const string &name) {
+        if (balances.find(name) == balances.end()) {
+            balances[name] = 0.0;
+        } else {
+            cout << name << " is already added." << endl;
+        }
+    }
+};
+int main(){}
