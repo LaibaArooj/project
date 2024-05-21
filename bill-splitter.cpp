8e8be4cb45c4604c74351bb18dc3ef262c6dcc97
LaibaArooj <144527015+LaibaArooj@users.noreply.github.com>
2024-05-21 12:11:12 +0500

    add expenses

--- bill-splitter.cpp
+++ bill-splitter.cpp
@@ -1,6 +1,42 @@
 #include<iostream>
 using namespace std;
+#include <unordered_map>
+#include <string>
+#include <vector>
+
+class BillSplitter {
+private:
+    unordered_map<string, double> balances;
+
+public:
+    void addFriend( string &name) {
+        if (balances.find(name) == balances.end()) {
+            balances[name] = 0;
+        } else {
+            cout << name << " is already added." << endl;
+        }
+    }
+
+    void addExpense( string &payer,  vector<string> &payees, double amount) {
+        if (balances.find(payer) == balances.end()) {
+            cout << "Payer " << payer << " not found." << endl;
+            return;
+        }
+
+        double share = amount / payees.size();
+
+        balances[payer] += amount;
+        for (const string &payee : payees) {
+            if (balances.find(payee) == balances.end()) {
+                cout << "Payee " << payee << " not found." << endl;
+                continue;
+            }
+            if (payee != payer) {
+                balances[payee] -= share;
+            }
+        }}
+    };
 int main(){
-cout<<"";
+
 return 0;
 }
