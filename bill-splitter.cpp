021775a969d9df4900183106af3c98bb45dfd6a8
mahnoor-chaudhary <chmano2545@gmail.com>
2024-05-23 10:12:02 +0500

    display

--- bill-splitter.cpp
+++ bill-splitter.cpp
@@ -35,8 +35,21 @@ public:
                 balances[payee] -= share;
             }
         }}
+        void displayBalance() const {
+        for (const auto &pair : balances) {
+            if (pair.second > 0) {
+                cout << pair.first << " needs to collect " << pair.second << " from others." << endl;
+            } else if (pair.second < 0) {
+                cout << pair.first << " needs to pay " << -pair.second << " to others." << endl;
+            } else {
+                cout << pair.first << " has no balance." << endl;
+            }
+        }
+    }
     };
 int main(){
 cout<<"";
+cout << "Final Balances:"<<endl;
+    splitter.displayBalance();
 return 0;
 }
